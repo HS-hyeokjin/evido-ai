54281cc7b878f040bd307e8ca96b64d5290e86fb
HS-hyeokjin <youngurwls@gmail.com>
2026-03-04 21:42:34 +0900

    feat : DB local/aws 연결

--- server/evido-api/evido-rag/app/db/session.py
+++ server/evido-api/evido-rag/app/db/session.py
@@ -3,5 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
 def get_engine() -> Engine:
-    dsn = os.getenv("DB_DSN", "mysql+pymysql://root:1234@localhost:3306/evido?charset=utf8mb4")
+    dsn = os.getenv(
+        "DB_DSN",
+        "mysql+pymysql://root:1234@localhost:3306/evido?charset=utf8mb4"
+    )
     return create_engine(dsn, pool_pre_ping=True, future=True)
