ff7af4463de647a2fba75d97ff91dffdd39d1d3b
HS-hyeokjin <youngurwls@gmail.com>
2026-06-22 15:42:28 +0900

    feat: /answer/stream 로직 추가(5)
    - streaming을 위한 RagPort,answerStream 함수 추가
    - Flux 리스폰 사용

--- server/evido-api/evido-api/src/main/java/com/evido/api/qa/application/port/out/RagPort.java
+++ server/evido-api/evido-api/src/main/java/com/evido/api/qa/application/port/out/RagPort.java
@@ -3,8 +3,13 @@ package com.evido.api.qa.application.port.out;
 import com.evido.api.qa.application.dto.ConversationContext;
 import com.evido.api.qa.application.port.in.command.AskCommand;
 import com.evido.api.qa.infrastructure.rag.dto.RagAnswerResponse;
+import com.evido.api.qa.infrastructure.rag.dto.RagStreamEvent;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface RagPort {
+
     Mono<RagAnswerResponse> answer(AskCommand command, ConversationContext context);
+
+    Flux<RagStreamEvent> answerStream(AskCommand command, ConversationContext context);
 }
